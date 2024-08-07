4c76b5604a93e92007e519c81ecde77494c99e0d
iribyz <byzovairina@gmail.com>
2024-08-07 22:56:13 +0300

    feat: add solution for reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let reverseNum = 0;
+  let origNum = Math.abs(n);
+  do {
+    reverseNum = reverseNum * 10 + origNum % 10;
+    origNum = Math.floor(origNum / 10);
+  } while (origNum > 0);
+  return reverseNum;
 }
